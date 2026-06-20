72e3cf453305573077d35d65237fa97c086b6632
ger-toshav <baily0515@gmail.com>
2026-06-20 21:48:38 +0000

    new mult function

--- text.py
+++ text.py
@@ -2,8 +2,13 @@ def count_to_ten():
     for i in range(1, 11):
         print(i)
 
+def multiply_1_to_5_by_4():
+    for i in range(1, 6):
+        print(i * 4)
+
 print("Hello World")
 
 # Example usage:
 count_to_ten()
+multiply_1_to_5_by_4()
 
